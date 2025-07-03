ec065cd795b7f9d8968add2a4d879938f24c87b1
Josiney <101153925+JosineyAlves@users.noreply.github.com>
2025-07-03 00:45:22 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const { exec } = require('child_process');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT;
 
 app.use(cors());
 
